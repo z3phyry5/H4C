f626cb772d6388e4f2cf045933caccdd63f5188a
JaiBhalla03 <101990594+JaiBhalla03@users.noreply.github.com>
2022-05-19 12:24:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-const hamburger = document.querySelector('.hamburger');
+const navslide=()=>{
+    const burger=document.querySelector('.burger');
+    const nav=document.querySelector('.menu');
+    const menu=document.querySelectorAll('.menu li');
+    //toggle nav
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
+        //animate links
+    menu.forEach((link,index)=>{
+        if(link.style.animation){
+            link.style.animation='';
+        }
+        else{
+            link.style.animation=`menuFade 0.5s ease forwards ${index/7+0.5}s`;
+        }
+        });
+        burger.classList.toggle('toggle');
+    });
+    
+}
 
-hamburger.addEventListener('click', function() {
-    this.classList.toggle
-})
+navslide(); 
